3d79bb02aeb7f993f39ddbbbf598b4922b140af1
13NOONE37 <oliwerklauze22@gmail.com>
2025-08-06 23:27:21 +0200

    collection in progress

---
+++ src/app/_components/collection.tsx
@@ -0,0 +1,9 @@
+export function Collection() {
+  return (
+    <section className="mt-30 md:mt-36 lg:mt-34">
+      <div className="bg-red bg-linear-to-b from-ui-neutral to-body-inverted bg-clip-text">
+        <h1 className="text-alternate-big text-transparent">our collection</h1>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Collection } from './_components/collection';
 import { Hero } from './_components/hero';
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Collection />
     </div>
   );
 }
