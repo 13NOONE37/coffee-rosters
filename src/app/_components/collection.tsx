a211065f26ff994ac3e935198ddc39ae0fa6a4ea
13NOONE37 <oliwerklauze22@gmail.com>
2025-08-14 15:09:05 +0200

    progress

--- src/app/_components/collection.tsx
+++ src/app/_components/collection.tsx
@@ -1,9 +1,53 @@
+import Image from 'next/image';
+
 export function Collection() {
+  const collection = [
+    {
+      name: 'Gran Espresso',
+      description:
+        'Light and flavorful blend with cocoa and black pepper for an intense experience',
+      imageURL: '/assets/home/desktop/image-gran-espresso.png',
+    },
+    {
+      name: 'Planalto',
+      description:
+        ' Brazilian dark roast with rich and velvety body, and hints of fruits and nuts',
+      imageURL: '/assets/home/desktop/image-planalto.png',
+    },
+    {
+      name: 'Piccollo',
+      description:
+        'Mild and smooth blend featuring notes of toasted almond and dried cherry',
+      imageURL: '/assets/home/desktop/image-piccollo.png',
+    },
+    {
+      name: 'Danche',
+      description:
+        'Ethiopian hand-harvested blend densely packed with vibrant fruit notes',
+      imageURL: '/assets/home/desktop/image-danche.png',
+    },
+  ];
+
   return (
     <section className="mt-30 md:mt-36 lg:mt-34">
-      <div className="bg-red bg-linear-to-b from-ui-neutral to-body-inverted bg-clip-text">
-        <h1 className="text-alternate-big text-transparent">our collection</h1>
+      <div className="bg-ui-neutral bg-linear-180 from-body-inverted/0 to-body-inverted/100 opacity-50 bg-clip-text">
+        <h1 className="text-alternate-big text-transparent text-center">
+          our collection
+        </h1>
       </div>
+      <ul className="flex flex-col gap-12 mt-3 md:-mt-14.5 lg:-mt-17.5">
+        {collection.map((coffee) => (
+          <li key={coffee.name} className="flex flex-col place-items-center">
+            <div className="w-[200px] h-[151px] relative">
+              <Image src={coffee.imageURL} alt={coffee.name} fill />
+            </div>
+            <h2 className="font-fraunces font-black text-[1.5rem] leading-[2rem] text-body mt-6">
+              {coffee.name}
+            </h2>
+            <p className="text-content text-body mt-4">{coffee.description}</p>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
