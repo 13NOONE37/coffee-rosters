fb943d1dc46963530ad77909a0693b9fc1bb00af
13NOONE37 <oliwerklauze22@gmail.com>
2025-08-15 21:56:42 +0200

    collection component done

--- src/app/_components/collection.tsx
+++ src/app/_components/collection.tsx
@@ -29,23 +29,30 @@ export function Collection() {
   ];
 
   return (
-    <section className="mt-30 md:mt-36 lg:mt-34">
-      <div className="relative ">
-        <h1 className="text-alternate-big text-ui-neutral text-center text-nowrap md:pt-7 md:pb-5 lg:pt-21.5 lg:pb-9.5">
+    <section className="flex flex-col place-items-center mt-30 md:mt-36 xl:mt-34">
+      <div className="max-w-full overflow-hidden relative -z-1">
+        <h1 className="text-alternate-big text-ui-neutral text-center text-nowrap md:pt-7 md:pb-5 xl:pt-21.5 xl:pb-9.5">
           our collection
         </h1>
         <div className="absolute inset-0 bg-linear-180 from-body-inverted/0 to-body-inverted/100"></div>
       </div>
-      <ul className="flex flex-col gap-12 mt-3 md:-mt-14.5 lg:-mt-17.5">
+      <ul className="flex flex-col xl:flex-row gap-12 md:gap-8 xl:gap-7 mt-3 md:-mt-7.5 xl:-mt-17.5">
         {collection.map((coffee) => (
-          <li key={coffee.name} className="flex flex-col place-items-center">
-            <div className="w-[200px] h-[151px] relative">
+          <li
+            key={coffee.name}
+            className="flex flex-col md:flex-row xl:flex-col align-items-center place-items-center md:place-items-start xl:place-items-center gap-6 md:gap-9 xl:gap-18"
+          >
+            <div className="w-[200px] aspect-[454/356] md:w-[255px] xl:w-[235px] relative">
               <Image src={coffee.imageURL} alt={coffee.name} fill />
             </div>
-            <h2 className="font-fraunces font-black text-[1.5rem] leading-[2rem] text-body mt-6">
-              {coffee.name}
-            </h2>
-            <p className="text-content text-body mt-4">{coffee.description}</p>
+            <div className="flex flex-col place-items-center md:place-items-start xl:place-items-center">
+              <h2 className="font-fraunces font-black text-[1.5rem] leading-[2rem] text-body">
+                {coffee.name}
+              </h2>
+              <p className="text-content text-body text-center md:text-left xl:text-center max-w-[282px] xl:max-w-[255px] mt-4 md:mt-6">
+                {coffee.description}
+              </p>
+            </div>
           </li>
         ))}
       </ul>
