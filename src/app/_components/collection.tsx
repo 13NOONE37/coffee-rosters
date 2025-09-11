a78d3f7b6918fbee15a0eb3154d74a79efbfeeec
13NOONE37 <oliwerklauze22@gmail.com>
2025-09-11 12:31:26 +0200

    moved static data out of component to avoid rerenders

--- src/app/_components/collection.tsx
+++ src/app/_components/collection.tsx
@@ -1,33 +1,33 @@
 import Image from 'next/image';
 
-export function Collection() {
-  const collection = [
-    {
-      name: 'Gran Espresso',
-      description:
-        'Light and flavorful blend with cocoa and black pepper for an intense experience',
-      imageURL: '/assets/home/desktop/image-gran-espresso.png',
-    },
-    {
-      name: 'Planalto',
-      description:
-        ' Brazilian dark roast with rich and velvety body, and hints of fruits and nuts',
-      imageURL: '/assets/home/desktop/image-planalto.png',
-    },
-    {
-      name: 'Piccollo',
-      description:
-        'Mild and smooth blend featuring notes of toasted almond and dried cherry',
-      imageURL: '/assets/home/desktop/image-piccollo.png',
-    },
-    {
-      name: 'Danche',
-      description:
-        'Ethiopian hand-harvested blend densely packed with vibrant fruit notes',
-      imageURL: '/assets/home/desktop/image-danche.png',
-    },
-  ];
+const collection = [
+  {
+    name: 'Gran Espresso',
+    description:
+      'Light and flavorful blend with cocoa and black pepper for an intense experience',
+    imageURL: '/assets/home/desktop/image-gran-espresso.png',
+  },
+  {
+    name: 'Planalto',
+    description:
+      ' Brazilian dark roast with rich and velvety body, and hints of fruits and nuts',
+    imageURL: '/assets/home/desktop/image-planalto.png',
+  },
+  {
+    name: 'Piccollo',
+    description:
+      'Mild and smooth blend featuring notes of toasted almond and dried cherry',
+    imageURL: '/assets/home/desktop/image-piccollo.png',
+  },
+  {
+    name: 'Danche',
+    description:
+      'Ethiopian hand-harvested blend densely packed with vibrant fruit notes',
+    imageURL: '/assets/home/desktop/image-danche.png',
+  },
+];
 
+export function Collection() {
   return (
     <section className="flex flex-col place-items-center mt-30 md:mt-36 xl:mt-34">
       <div className="max-w-full overflow-hidden relative -z-1">
